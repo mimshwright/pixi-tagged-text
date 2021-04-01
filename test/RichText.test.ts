de973dcd673b3a40a5f1b3ed5a2ea6908998a03d
Mims H. Wright <mims@mimswright.com>
2021-04-01 15:21:51 -0700

    updated tests to add performance check

--- test/RichText.test.ts
+++ test/RichText.test.ts
@@ -44,7 +44,7 @@ describe("RichText", () => {
         });
       });
 
-      describe("skipUpdates", () => {
+      describe("skipUpdates & skipDraw", () => {
         const text = "Test <b>test</b>";
         const control = new RichText(text, style);
         const skipUpdates = new RichText(text, style, {
@@ -98,6 +98,54 @@ describe("RichText", () => {
           expect(control.tokens[0]).toHaveLength(3);
           control.update(false);
           expect(control.textFields).toHaveLength(3);
+        });
+
+        const REPS = 50;
+        describe(`performace of skipping draw and updates. Updating string ${REPS} times.`, () => {
+          // Performance
+          const editText = (textField: RichText) => {
+            textField.text = "";
+            for (let i = 0; i < REPS; i++) {
+              textField.text += `${i} `;
+            }
+          };
+
+          const control = new RichText();
+          const skipDraw = new RichText("", {}, { skipDraw: true });
+          const skipUpdates = new RichText("", {}, { skipUpdates: true });
+
+          let startTime = new Date().getTime();
+          editText(control);
+          let endTime = new Date().getTime();
+          const timeControl = endTime - startTime;
+
+          startTime = new Date().getTime();
+          editText(skipDraw);
+          skipDraw.draw();
+          endTime = new Date().getTime();
+          const timeSkipDraw = endTime - startTime;
+
+          startTime = new Date().getTime();
+          editText(skipUpdates);
+          skipUpdates.update();
+          endTime = new Date().getTime();
+          const timeSkipUpdates = endTime - startTime;
+
+          it(`Default is slow AF! ${timeControl}ms`, () => {
+            expect(timeControl).toBeGreaterThanOrEqual(500);
+          });
+          it(`skipDraw should be faster than default. ${timeSkipDraw}ms`, () => {
+            expect(timeSkipDraw).toBeLessThan(timeControl);
+          });
+          it(`skipUpdates should be faster than control and skipDraw. ${timeSkipUpdates}ms < ${timeSkipDraw}ms < ${timeControl}ms`, () => {
+            expect(timeSkipUpdates).toBeLessThan(timeControl);
+            expect(timeSkipUpdates).toBeLessThan(timeSkipDraw);
+          });
+          it(`In fact, skipUpdates it's pretty fast! ${timeSkipUpdates}ms`, () => {
+            expect(timeSkipUpdates).toBeLessThan(50);
+          });
+
+          console.log({ timeControl, timeSkipDraw, timeSkipUpdates });
         });
       });
     });
