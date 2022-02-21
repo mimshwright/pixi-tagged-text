8f40a7dddeb1095d9c440b1a18e36b7bc4b68820
Mims H. Wright <mims@mimswright.com>
2022-02-20 21:54:33 -0800

    Moved to jest-electron

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
-  setupFiles: ["jest-canvas-mock"],
+  runner: "@jest-runner/electron",
+  testEnvironment: "@jest-runner/electron/environment",
 };
